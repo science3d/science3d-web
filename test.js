ae5f59d347b4b9ebaaf48560fc30fb37476d3c1f
Maxim Litvak <dr.m.litvak@gmail.com>
2020-01-28 14:47:12 +0200

    script.js doesnt work

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
 //console.log("Hello!");
+const navButton = document.querySelector('.nav-button');
+const navOpen = document.querySelector('.nav-open');
+
+
 const tw = TweenLite.to(".cover",1,{width: "40%"});
 
 const tl = new TimelineLite({ paused: true, reversed: true });
